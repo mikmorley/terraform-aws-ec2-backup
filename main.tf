ece7732b665655ae6521686a5083a3700d538acf
Michael Morley <michael-morley@pluralsight.com>
2023-07-07 08:45:56 +1000

    move variables to variables file

--- main.tf
+++ main.tf
@@ -1,36 +1,3 @@
-variable "name" {
-  type = string
-}
-
-variable "environment" {
-  type = string
-}
-
-variable "region" {
-  type = string
-}
-
-variable "timeout" {
-  type    = number
-  default = 60
-}
-
-variable "backup_tag" {
-  type        = string
-  default     = "Backup"
-  description = "The EC2 Instance Tag that will be checked for the 'yes' value, to backup."
-}
-
-variable "backup_retention" {
-  type        = number
-  default     = 30
-  description = "The number of days a backup will be kept."
-}
-
-variable "schedule_expression" {
-  description = "Scheduling expression for triggering the Lambda Function using CloudWatch events. For example, cron(0 20 * * ? *) or rate(5 minutes)."
-}
-
 resource "aws_iam_role" "default" {
   name = "${var.name}-${var.region}"
   path = "/service-role/"
